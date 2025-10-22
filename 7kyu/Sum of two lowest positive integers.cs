c7cb1dcb2fba852025ee0e573fa39f42b1d168cb
Aung_Lin <linoscar724@gmail.com>
2025-10-22 13:13:30 +0630

    [Fix]: sumTwoSmallestNumbers 7kyu

---
+++ 7kyu/Sum of two lowest positive integers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarConsoleApp._7kyu
+{
+    public class Sum_of_two_lowest_positive_integers
+    {
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {
+            
+            int smallest = numbers.Min();
+            
+            numbers = numbers.Where(x => x != smallest).ToArray();
+            int secsmallest = numbers[0];
+            foreach (int i in numbers)
+            {
+                if(secsmallest > i)
+                {
+                    secsmallest = i;
+                }
+            }
+            
+            return smallest + secsmallest;
+        }
+
+        public static int sumTwoSmallestNumbersV2(int[] numbers)
+        {
+
+            return numbers.OrderBy(x=>x).Take(2).Sum();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,14 @@
 ﻿using CodeWarConsoleApp._6kyu;
+using CodeWarConsoleApp._7kyu;
 
 public class Program
 {
     public static void Main()
     {
 
-        Replace_With_Alphabet_Position.AlphabetPosition("The sunset sets at twelve o' clock.");
-        //Replace_With_Alphabet_Position.AlphabetPosition("The narwhal bacons at midnight.");
-        //Replace_With_Alphabet_Position.AlphabetPosition("sagkakyxmhlepb");
-
-        Replace_With_Alphabet_Position.AlphabetPositionVersion2("The sunset sets at twelve o' clock.");
-
-        Console.WriteLine(MorseCodeDecoder.Decode(".... . -.--   .--- ..- -.. ."));
-
-
-        Console.WriteLine("Unique In Order",Unique_In_Order.UniqueInOrderYield("AAAABBBCCDAnullABBB"));
-
-
-
-        int[] smth = { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 };
-        Find_The_Parity_Outlier.Find(smth);
-
-
+       
+        int[] number = { 5, 8, 12, 19, 22 };
+        Console.WriteLine(Sum_of_two_lowest_positive_integers.sumTwoSmallestNumbers(number));
 
     }
 
